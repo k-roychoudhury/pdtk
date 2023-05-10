51999b7195d414ed2f9c9e01924b116941dd5ba0
Kunal Roychoudhury <lanuks97@gmail.com>
2023-05-10 12:30:02 +0530

    refactor: move code around

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,8 @@ r""" tests.conftest module """
 
 
 # importing standard modules ==================================================
-from os.path import dirname, join
 import sys
-
+from os.path import dirname, join
 # adding the 'src' directory to path - HACK
 sys.path.append(
     join(dirname(dirname(__file__)), "src")
