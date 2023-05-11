021423ba3473b6bf9b4045e98dded2959b073697
Kunal Roychoudhury <lanuks97@gmail.com>
2023-05-11 11:29:37 +0530

    refactor: utility network tests moved fixtures to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,23 @@ from os.path import dirname, join
 sys.path.append(
     join(dirname(dirname(__file__)), "src")
 )
+
+
+# importing third-party modules ===============================================
+import pytest
+import pytest_asyncio
+from aiohttp import ClientSession
+from requests import Session
+
+
+# test fixtures ===============================================================
+@pytest_asyncio.fixture
+async def async_client_session() -> ClientSession:
+    async with ClientSession() as session:
+        yield session
+
+
+@pytest.fixture(scope='session')
+def sync_client_session() -> Session:
+    with Session() as session:
+        yield session
