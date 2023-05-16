dbfa62448674ce1a26f38fd4cb64d8653e75a110
Kunal Roychoudhury <lanuks97@gmail.com>
2023-05-16 13:10:46 +0530

    refactor: use common http session

--- pdtk/config.py
+++ pdtk/config.py
@@ -1,6 +1,28 @@
 r""" pdtk.config module """
 
 
+# importing third-party modules ===============================================
+from requests import (
+    Session,
+)
+
+
+# module variables ============================================================
+module_http_session: Session | None = None
+
+
 # module constants ============================================================
 BASE_URL_FAMILIZER: str = "https://www.familyizer.com/getfamily5.lc"
 BASE_URL_GOOGLE_PATENTS: str = "https://patents.google.com/xhr"
+
+
+# method definitions ==========================================================
+def get_http_session() -> Session:
+    r""" Module Method: Get Http Session 
+    - returns:
+        - an object of type `requests.Session`
+    """
+    global module_http_session
+    if module_http_session is None:
+        module_http_session = Session()
+    return module_http_session
