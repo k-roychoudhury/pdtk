e780f63f27813b799e849422a8547d66a223009b
Kunal Roychoudhury <lanuks97@gmail.com>
2023-05-24 14:43:13 +0530

    add: exceptions module

---
+++ pdtk/google_patents/exceptions.py
@@ -0,0 +1,10 @@
+r""" pdtk.google_patents.exceptions module """
+
+
+# class definitions =====================================================================
+class DataError(Exception):
+    pass # end of DataError
+
+
+class ContentError(DataError):
+    pass # end of ContentError
